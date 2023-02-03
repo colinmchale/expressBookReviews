2d3553abdc637dc7e7e8593b54c0bb1d1fe6e744
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2023-02-03 16:28:01 -0600

    changing all function to async

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require("axios");
 
 const doesExist = (username) => {
   let userswithsamename = users.filter((user) => {
@@ -15,7 +16,7 @@ const doesExist = (username) => {
   }
 };
 
-public_users.post("/register", (req, res) => {
+public_users.post("/register", async (req, res) => {
   //Write your code here
   const username = req.body.username;
   const password = req.body.password;
@@ -33,72 +34,56 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
+public_users.get("/", async (req, res) => {
   //Write your code here
-  try {
-    res.send(JSON.stringify({ books }, null, 4));
-  } catch (error) {
-    res.status(500).send(error);
-  }
+  res.send(JSON.stringify({ books }, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async (req, res) => {
   //Write your code here
-  try {
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async (req, res) => {
   //Write your code here
-  try {
-    const reqAuthor = req.params.author;
-    const allBooks = Object.values(books);
-    let filteredBooks = {};
-    for (const { author, title, reviews } of allBooks) {
-      if (author === reqAuthor) {
-        filteredBooks = {
-          author: author,
-          title: title,
-          reviews: reviews,
-        };
-      }
+  const reqAuthor = req.params.author;
+  const allBooks = Object.values(books);
+  let filteredBooks = {};
+  for (const { author, title, reviews } of allBooks) {
+    if (author === reqAuthor) {
+      filteredBooks = {
+        author: author,
+        title: title,
+        reviews: reviews,
+      };
     }
-    res.send(filteredBooks);
-  } catch (error) {
-    res.status(500).send(error);
   }
+  res.send(filteredBooks);
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async (req, res) => {
   //Write your code here
-  try {
-    const reqTitle = req.params.title;
-    const allBooks = Object.values(books);
-    let filteredBooks = {};
-    for (const { author, title, reviews } of allBooks) {
-      if (title === reqTitle) {
-        filteredBooks = {
-          author: author,
-          title: title,
-          reviews: reviews,
-        };
-      }
+  const reqTitle = req.params.title;
+  const allBooks = Object.values(books);
+  let filteredBooks = {};
+  for (const { author, title, reviews } of allBooks) {
+    if (title === reqTitle) {
+      filteredBooks = {
+        author: author,
+        title: title,
+        reviews: reviews,
+      };
     }
-    res.send(filteredBooks);
-  } catch (error) {
-    res.status(500).send(error);
   }
+  res.send(filteredBooks);
 });
 
 //  Get book review
-public_users.get("/review/:isbn", function (req, res) {
+public_users.get("/review/:isbn", async (req, res) => {
   //Write your code here
   const isbn = req.params.isbn;
   res.send(books[isbn].reviews);
